571911336b058f0c09539b45c2b9d838b863e7d9
Tjark Miener <tjark.miener4692@googlemail.com>
2019-05-24 15:52:19 +0200

    added gloryduck class containing all necessary information

---
+++ likelihood_combiner/gloryduck.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python'
+
+class gloryduckInfo:
+    def __init__(self):
+        ######################################################
+        #              Modify information here:              #
+        ######################################################
+
+        # List of the collaborations:
+        self.collaborations = ['MAGIC','HESS','FermiLAT','VERITAS','HAWC']
+
+        # List of sources:
+        self.sources = ['Booetes1', 'Carina', 'ComaBerenices', 'Draco', 'Fornax', 'UrsaMajorII', 'UrsaMinor', 'Sagittarius', 'Sculptor', 'Segue1', 'Willman1']
+
+        # List of annihilation channels:
+        self.channels = ['bb', 'tautau', 'mumu', 'WW', 'gammagamma', 'ZZ','ee']
+    
+        #######################################################
+        #                     Thank you!                      #
+        #######################################################
