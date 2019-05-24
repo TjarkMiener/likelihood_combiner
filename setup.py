9a073c36178ebdfeb30c78c66d7f031d5b60f7ba
Tjark Miener <tjark.miener4692@googlemail.com>
2019-05-24 12:34:40 +0200

    added installation via conda

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='lklcom',
+      version='0.0.1',
+      description='LikelihoodCombiner combines likelihoods from different experiments.',
+      url='https://github.com/TjarkMiener/likelihood_combiner',
+      license='BSD-3-Clause',
+      packages=['likelihood_combiner'],
+      include_package_data=True,
+      dependencies=[],
+      dependency_links=[],
+      zip_safe=False)
