2a4792377535a32dcb067e1a416e124f839d1fc1
Tjark Miener <tjark.miener4692@googlemail.com>
2019-06-05 16:31:26 +0200

    add ability to combine limits

--- likelihood_combiner/combiner.py
+++ likelihood_combiner/combiner.py
@@ -35,9 +35,10 @@ def run_combiner(config):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    writer = gloryduckWriter()
-
-    writer.convert_txts2hdf5(hdf5file,data_dir)
+    if config['Data']['overwrite_hdf5file']:
+        writer = gloryduckWriter()
+        writer.convert_txts2hdf5(hdf5file,data_dir)
+        del writer
 
     channels = config['Configuration']['channels']
     sources = config['Configuration']['sources']
@@ -53,13 +54,14 @@ def run_combiner(config):
         collaborations = np.array(gloryduck.collaborations)
 
     reader = gloryduckReader()
-
     tstables, massvals = reader.read_gloryduck_tstables(hdf5file,channels,sources,collaborations)
+    del reader
 
     for channel in channels:
         for source in sources:
             mass_ref = None
             tstable_ref = None
+            combined_ts = None
             for key,mass,tstable in zip(massvals.keys(),massvals.values(),tstables.values()):
                 if channel in key and source in key:
                     # Checking that all ranges (mass and sigmav) and the J-Factor (first element of the mass arrays) are equal
@@ -68,7 +70,7 @@ def run_combiner(config):
                         mass_key_ref = key
                     else:
                         if mass[0] != mass_ref[0]:
-                            raise ValueError("The J-Factor value have to be equal! Discrepancy in '{}.txt' and '{}.txt'".format(key,mass_key_ref))
+                            print("Warning: Discrepancy in the J-Factor value of '{}.txt' and '{}.txt'! Make sure each collabration took the right J-Factor value from the GS table.".format(key,mass_key_ref))
                         if (mass[1:]!=mass_ref[1:]).any():
                             raise ValueError("The mass values have to be equal! Discrepancy in '{}.txt' and '{}.txt'".format(key,mass_key_ref))
             
@@ -78,11 +80,19 @@ def run_combiner(config):
                     else:
                         if (tstable[0]!=tstable_ref).any():
                             raise ValueError("The sigma values have to be equal! Discrepancy in '{}.txt' and '{}.txt'".format(key,tstable_key_ref))
-                    
-                    limits = compute_sensitivity(tstable[0], tstable[1:])
+                    sigmav = tstable[0]
+                    if combined_ts is None:
+                        combined_ts = np.zeros((tstable.shape[0]-1, tstable.shape[1]))
+                    for i in np.arange(tstable.shape[0]-1):
+                        # Combining the likelihood values
+                        combined_ts[i] += tstable[i+1]
+
+                    # Calculating the limits
+                    limits = compute_sensitivity(sigmav, tstable[1:])
                     print(limits)
-    del writer
-    del reader
+
+            combined_limit = compute_sensitivity(sigmav, combined_ts)
+            print(combined_limit)
 
 if __name__ == "__main__":
     
