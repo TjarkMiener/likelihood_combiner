50231c9bebf95c350201461d99c058205cde5e4b
Tjark Miener <tjark.miener4692@googlemail.com>
2019-06-11 16:24:06 +0200

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lklcom',
-      version='0.0.1',
+      version='0.1.0',
       description='LikelihoodCombiner combines likelihoods from different experiments.',
       url='https://github.com/TjarkMiener/likelihood_combiner',
       license='BSD-3-Clause',
