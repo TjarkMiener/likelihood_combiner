3129a521d78992446a6a5c0a774a293608ccb9e9
Tjark Miener <tjark.miener4692@googlemail.com>
2021-01-10 00:15:37 +0100

    fix setup,py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup, find_packages
+from os import path
+from likelihood_combiner.version import *
+
+# Taken from https://github.com/me-manu/gammaALPs/blob/master/setup.py
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='lklcom',
-      version='0.4.1',
-      description='LikelihoodCombiner combines likelihoods from different experiments.',
+      version=get_version_pypi(),
+      author="Tjark Miener",
+      author_email="tmiener@ucm.es",
+      description='LikelihoodCombiner combines DM-related likelihoods from different experiments.',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       url='https://github.com/TjarkMiener/likelihood_combiner',
       license='BSD-3-Clause',
       packages=['likelihood_combiner'],
