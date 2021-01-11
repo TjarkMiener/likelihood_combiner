032691cf60c47902ddfb47b0d78a1a261dc60926
Tjark Miener <tjark.miener4692@googlemail.com>
2021-01-11 23:58:43 +0100

    fix conda upload

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -4,7 +4,7 @@ USER=tmiener
 
 OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
-conda config --set anaconda_upload no
+conda config --set anaconda_upload yes
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
