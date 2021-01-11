c5403de5ed960afde908b7638a8c47b19b10a454
Tjark Miener <tjark.miener4692@googlemail.com>
2021-01-12 00:26:58 +0100

    fix conda upload^3

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -4,10 +4,10 @@ USER=tmiener
 
 OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
-conda config --set anaconda_upload yes
+conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-LKLCOM_VERSION=`python -c "import likelihood_combiner; print(likelihood_combiner.__version__);"
+LKLCOM_VERSION=`python -c "import likelihood_combiner; print(likelihood_combiner.__version__);"`
 
 export VERSION=$LKLCOM_VERSION
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$LKLCOM_VERSION.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$LKLCOM_VERSION-0.tar.bz2 --force
