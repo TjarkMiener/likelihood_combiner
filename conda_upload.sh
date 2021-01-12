dbdb7e882cb8dbb2428babe7a849859ae77844c9
Tjark Miener <tjark.miener4692@googlemail.com>
2021-01-12 02:00:21 +0100

    fix conda auto upload

--- conda_upload.sh
+++ conda_upload.sh
@@ -6,7 +6,7 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-LKLCOM_VERSION=`python -c "import likelihood_combiner; print(likelihood_combiner.__version__);"`
+LKLCOM_VERSION=`python -c "import likelihood_combiner; print(likelihood_combiner.__version__.split("+")[0]);"`
 
 export VERSION=$LKLCOM_VERSION
 conda build .
