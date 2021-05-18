4f546878852a2069530b4de97ee282c0fcc3287d
TjarkMiener <tjark.miener4692@googlemail.com>
2021-05-18 14:14:41 +0200

    bug fixed
    pass the sigmav range bounds to compute_Jnuisance()

--- likelihood_combiner/combiner.py
+++ likelihood_combiner/combiner.py
@@ -88,9 +88,8 @@ def combiner(sigmav_range,
                         combined_source_ts[source_mass] += ts_values
                         if jnuisance:
                             combined_source_ts_Jnuisance[source_mass] += ts_values
-                    
                     if jnuisance and logJ[source][collaboration] != 0.0:
-                        combined_source_ts_Jnuisance[source_mass] = LklCom_jfactor_class.compute_Jnuisance(sigmav_range, combined_source_ts_Jnuisance[source_mass], np.float32(logJ[source][collaboration]))
+                        combined_source_ts_Jnuisance[source_mass] = LklCom_jfactor_class.compute_Jnuisance(sigmav_range, combined_source_ts_Jnuisance[source_mass], np.float32(logJ[source][collaboration]), np.min(sigmav_range), np.max(sigmav_range))
 
             # Combine ts values for all dSphs
             for mass in total_masses:
